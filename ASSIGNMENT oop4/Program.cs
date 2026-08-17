26ff83c655a7f0125acab3162f0321a9bc9b5050
shr0ukgomaa33 <shroukgomaa8787@gmail.com>
2026-08-17 15:05:31 +0300

    Q1

--- ASSIGNMENT oop4/Program.cs
+++ ASSIGNMENT oop4/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-          
+            #region Q1 
+
+            // A) Abstraction is the concept of hiding unnecessary implementation details and showing only the essential features of an object to the user.
+
+            // B) Abstraction is one of the four pillars of OOP because it reduces complexity by hiding unnecessary details and focusing on what an object does rather than how it does it. It also makes code easier to understand, maintain, and reuse.
+
+            #endregion
+
+
+
+
         }
     }
 }
